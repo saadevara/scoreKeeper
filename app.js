a1c5df6a2fc1e4d4f604a533bbe359825184b827
Saad Evara <saadevara@gmail.com>
2022-02-08 12:13:26 +0530

    Customize looks

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 3;
+let winningScore = 7;
 let isGameOver = false;
 
 p1Button.addEventListener('click', () => {

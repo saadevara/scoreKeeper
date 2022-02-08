fafa967b09d951d3f161072794206d436d6e6b28
Saad Evara <saadevara@gmail.com>
2022-02-08 14:30:25 +0530

    Bug fixes

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 const p1 = {
     score: 0,
-    button : document.querySelector('#p1Button'),
-    display : document.querySelector('#p1Display')
-
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
 }
-
 const p2 = {
     score: 0,
-    button : document.querySelector('#p2Button'),
-    display : document.querySelector('#p2Display')
-
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
 }
 
-const resetButton = document.querySelector('#reset');
+const resetButton = document.querySelector("#reset");
 const winningScoreSelect = document.querySelector('#playto');
-
-let winningScore = 3 ;
+let winningScore = 3;
 let isGameOver = false;
 
 function updateScores(player, opponent) {
-     if (!isGameOver) {
-        player.score+=1; 
+    if (!isGameOver) {
+        player.score += 1;
         if (player.score === winningScore) {
             isGameOver = true;
             player.display.classList.add('has-text-success');
@@ -32,28 +28,28 @@ function updateScores(player, opponent) {
     }
 }
 
-p1.button.addEventListener('click', () => {
-    updateScores(p1, p2);
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
 })
 
-p2.button.addEventListener("click", () => {
-  updateScores(p2,p1)
-});
 
-winningScoreSelect.addEventListener('change', function()  {
+winningScoreSelect.addEventListener('change', function () {
     winningScore = parseInt(this.value);
     reset();
 })
 
-resetButton.addEventListener('click',reset)
+resetButton.addEventListener('click', reset)
 
 function reset() {
     isGameOver = false;
     for (let p of [p1, p2]) {
         p.score = 0;
         p.display.textContent = 0;
-        p.display.classList.remove('has-text-succes', 'has-text-danger');
+        p.display.classList.remove('has-text-success', 'has-text-danger');
         p.button.disabled = false;
     }
-  
 }

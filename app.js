294b604f0dff3da34e3d640e4f1f701e23efba3e
Saad Evara <saadevara@gmail.com>
2022-02-08 10:12:08 +0530

    Add select button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const p2Button = document.querySelector('#p2Button')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
 const reset = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -33,7 +34,7 @@ reset.addEventListener('click', () => {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
-    p1Display.textContent = p1Score;
-    p2Display.textContent = p2Score;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
 })
 

21e5c068f25f75936d4c3668d36cf0635bdc1ad4
Saad Evara <saadevara@gmail.com>
2022-02-08 11:47:50 +0530

    Add Bulma CSS

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const p1Button = document.querySelector('#p1Button')
 const p2Button = document.querySelector('#p2Button')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
-const reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
 const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', () => {
@@ -30,11 +30,17 @@ p2Button.addEventListener("click", () => {
   }
 });
 
-reset.addEventListener('click', () => {
+winningScoreSelect.addEventListener('change', () => {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click',reset)
+
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-})
-
+}
